a5dbb01a96c1449c3196c27752eee0fc9ccfa9dd
Сергей Пчелинцев <tiksten@outlook.com>
2020-12-03 23:16:01 +0300

    BOOM Update
    Added Physics kaboom

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class Bomb : MonoBehaviour
 {
 
     public GameObject hitEffect;
     public int damage = 40;
-
+    public float radius = 5f;
+    public float force = 700f;
     void OnTriggerEnter(Collider hitInfo)
     {
         //PlayerHealth enemy = hitInfo.GetComponent<PlayerHealth>();
@@ -15,12 +16,26 @@ public class Bullet : MonoBehaviour
         //{
         //    enemy.TakeDamage(damage);
         //}
+        Explode();
         GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
         Destroy(effect, 0.5f);
         Destroy(gameObject);
     }
     void Start()
     {
-        Destroy(gameObject, 5f);
+        Destroy(gameObject, 10f);
+    }
+
+    void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+        foreach (Collider nearbyObject in colliders)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(force, transform.position, radius);
+            }
+        }
     }
 }
