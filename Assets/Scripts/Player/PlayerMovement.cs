8de4decc908d057324b01578a32bb4fe9cdbf90b
Tiksten <tiksten@outlook.com>
2021-03-11 22:48:25 +0300

    1a (1.1.2)
    Added ability to crouch and run
    Now default layer is not ground

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -11,16 +11,22 @@ public class PlayerMovement : MonoBehaviour
     private bool stepPlayed;
     public float timeBetweenSteps;
 
-    public float speed = 12f;
+    public float speed = 8f;
+    public float runSpeed = 14f;
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
 
+    public Transform cellingCheck;
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
+    public Animator handsAnim;
 
     Vector3 velocity;
     bool isGrounded;
+
+    [HideInInspector]
+    private Vector3 previousPos;
 
     // Update is called once per frame
     void Update()
@@ -35,16 +41,61 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 move = transform.right * x + transform.forward * z;
 
-        controller.Move(move * speed * Time.deltaTime);
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        //Moving
+
+
+        if (Input.GetKey("left shift"))
+        {
+            controller.Move(move * runSpeed * Time.deltaTime);
+            handsAnim.SetBool("Running", true);
+        }
+        else
+        {
+            controller.Move(move * speed * Time.deltaTime);
+            handsAnim.SetBool("Running", false);
+            handsAnim.SetBool("Walking", true);
+        }
+
+        if (previousPos == gameObject.transform.position)
+        {
+            handsAnim.SetBool("Running", false);
+            handsAnim.SetBool("Walking", false);
+        }
+
+        if (Input.GetKeyDown("space") && isGrounded)
             velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+
+
+        //Crouching
+
+
+        if (Input.GetKeyDown("left ctrl") && transform.localScale != new Vector3(1, 0.4f, 1))
+        {
+            transform.localScale = new Vector3(1, 0.4f, 1);
+            foreach (Transform i in transform)
+                i.localScale = new Vector3(1, i.localScale.y * 2.5f, 1);
+        }
+
+        else if (!Input.GetKey("left ctrl") && !Physics.CheckSphere(cellingCheck.position, 0.5f, groundMask) && transform.localScale != new Vector3(1, 1, 1))
+        {
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
+            gameObject.transform.position += new Vector3(0, gameObject.transform.position.y + 1, 0);
+            foreach (Transform i in transform)
+                i.localScale = new Vector3(1, i.localScale.y / 2.5f, 1);
+        }
+
+
+        //Other
+
 
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
 
         StepSoundPlay();
+
+        previousPos = gameObject.transform.position;
     }
 
     private void StepSoundPlay()
